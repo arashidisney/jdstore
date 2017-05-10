2fd45a9c2a6e5c9c4dc9474489a506e650855750
蒔苗ヒナ <hina@shimiaohinade-MacBook-Pro.local>
2017-05-10 15:42:43 +0900

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   namespace :admin do
   resources :products
 
-
   end
-
-  resources :products do
+  
   resources :carts
+  resources :products do
   member do
      post :add_to_cart
    end
